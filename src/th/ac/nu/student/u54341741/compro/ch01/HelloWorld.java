83e7676f92bcd99e2ca83286169eb2742abd01d5
supagorn saekue <cream_1923@hotmail.com>
2012-06-19 14:23:44 +0700

    เพิ่มข้อความ Programming is fun

--- src/th/ac/nu/student/u54341741/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/student/u54341741/compro/ch01/HelloWorld.java
@@ -10,7 +10,8 @@ package th.ac.nu.student.u54341741.compro.ch01;
  */
 public class HelloWorld {
     public static void main(String [] args){
-     System.out.println("Hello,World");   
+     System.out.println("Hello,World");
+     System.out.println("Programming is fun");
     }
     
 }
