e5e49fbb986981b0e98aea8650b5dfa027c2c1b4
supagorn saekue <cream_1923@hotmail.com>
2012-06-19 14:18:30 +0700

    สร้าง HelloWorld.java

---
+++ src/th/ac/nu/student/u54341741/compro/ch01/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch01;
+
+/**
+ *
+ * @author GHz
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+     System.out.println("Hello,World");   
+    }
+    
+}
