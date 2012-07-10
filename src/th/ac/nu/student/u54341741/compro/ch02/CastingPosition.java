ec63af2a3504506c13dd0f8d17be82a2da7a8010
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 20:12:00 +0700

    CastingPosition

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/CastingPosition.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class CastingPosition {
+    
+    public static void main(String[] args) {
+        float x = 1.3f;
+        float y = 1.8f;
+        System.out.println((int) (x + y));       // 3
+        System.out.println((int) x + (int) y);   // 2
+      }    
+}
