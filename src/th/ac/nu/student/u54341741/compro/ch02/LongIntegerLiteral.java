3740fc4fc05c258b196143d9dae432fcc7eb228e
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 20:15:01 +0700

    LongIntegerLiteral

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/LongIntegerLiteral.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class LongIntegerLiteral {
+    
+    public static void main(String[] args) {
+        System.out.println(2147483647);
+        System.out.println(-2147483648);
+        System.out.println(9223372036854775807L);
+        System.out.println(-9223372036854775808L);
+    }   
+}
