bcae20ef416f4ad359b5cbeb0c387489d0b3af19
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 20:11:12 +0700

    BooleanLiteral

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/BooleanLiteral.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class BooleanLiteral {
+    
+    public static void main(String[] args){
+        System.out.println(true); // true
+        System.out.println(false); // false
+        System.out.println(1 < 2); // true
+        System.out.println('A' == 'B'); // false
+    }
+    
+}
