5ee40f493e826b5610857846cef5c652377c6b58
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 20:11:25 +0700

    BooleanLogicalOperator

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/BooleanLogicalOperator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class BooleanLogicalOperator {
+    
+    public static void main(String[] args) {
+        boolean t = true, f = false;
+        System.out.println(t & f);     // false
+        System.out.println(t | f);     // true
+        System.out.println(t ^ f);     // true
+        System.out.println(!t);        // false
+     } 
+}
