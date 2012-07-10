a08dac3d43471213948b4a691659223de426cb78
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 19:37:24 +0700

    Example

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/Example01.java
@@ -0,0 +1,17 @@
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class Example01 {
+    public static void main (String []args){
+        
+        double Number = 2 ;
+        
+        System.out.println("Square power is : " + Math.pow(Number ,2));
+        System.out.println("Cube   power is : " + Math.pow(Number ,3));
+        System.out.println("Fourth power is : " + Math.pow(Number, 4));
+        
+    } 
+}
