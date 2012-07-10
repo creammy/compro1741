333016d6132113ee87b61b5822baca9e448a23b5
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 20:13:46 +0700

    FloatLiteral

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/FloatLiteral.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class FloatLiteral {
+    
+    public static void main(String[] args) {
+        System.out.println(0.12345);
+        System.out.println(1.2345E-1);
+        System.out.println(1.2345e-1);
+        }
+    }
