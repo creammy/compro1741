7e59524cffa34771909be293d8326fb729bfd71b
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 20:13:35 +0700

    FloatError

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/FloatError.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class FloatError {
+    
+    public static void main(String[] args) {
+        // float f = 1.0; <-- Error
+        float x = (float) 1.0;
+        float y = 1.0F;
+    }    
+}
