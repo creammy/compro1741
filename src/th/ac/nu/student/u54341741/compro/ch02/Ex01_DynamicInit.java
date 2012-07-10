c88d9be26b4673859d5c0d3b923ec861206a2460
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 20:12:58 +0700

    Ex01_DynamicInit

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/Ex01_DynamicInit.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class Ex01_DynamicInit {
+    public static void main(String [] args){
+        System.out.println(args.length);
+    }
+    
+}
