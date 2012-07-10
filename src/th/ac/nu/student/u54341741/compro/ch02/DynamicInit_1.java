2bdedc3981774250729cd013a6553a151acb6808
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 20:12:46 +0700

    DynamicInit_1

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/DynamicInit_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class DynamicInit_1 {
+    
+    public static void main(String [] args){
+        if (args.length == 2) {
+    
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println(c);
+        }
+        }  
+}
