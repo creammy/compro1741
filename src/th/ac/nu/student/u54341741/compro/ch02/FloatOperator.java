8c29a369cd699911e8a1a2555ee0f2298d103b60
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 20:13:56 +0700

    FloatOperator

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/FloatOperator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class FloatOperator {
+    
+    public static void main(String[] args) {
+        System.out.println(1.0 + 2.0);       // 3.0 
+        System.out.println(2.0 - 3.0);       // -1.0
+        System.out.println(3.0 * 4.0);       // 12.0
+        System.out.println(16.0 / 5.0);      // 3.2
+        System.out.println(16.0 % 5.0);      // 1.0
+    }    
+}
