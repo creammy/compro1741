17c4e35fac6c24d2c1c640d01d54a6212fbda0a3
supagorn saekue <cream_1923@hotmail.com>
2012-07-10 20:07:53 +0700

    ArrayDeclaration

---
+++ src/th/ac/nu/student/u54341741/compro/ch02/ArrayDeclaration.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341741.compro.ch02;
+
+/**
+ *
+ * @author GHz
+ */
+public class ArrayDeclaration {
+    
+    public static void main (String [] args){
+        int numbers[] = {1, 2, 3, 5, 8, 13};
+        System.out.println("First member of this array is "+ numbers[0]);
+        System.out.println("Last member of this array is "+ numbers[numbers.length - 1]);
+    }  
+}
